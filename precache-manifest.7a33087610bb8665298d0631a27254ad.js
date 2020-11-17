6d3333723ccbf5a54b57ef8da71d4be6c3286d24
AndresContreras96 <63315471+AndresContreras96@users.noreply.github.com>
2020-11-17 01:06:51 -0600

    Updates

---
+++ precache-manifest.7a33087610bb8665298d0631a27254ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8473f75b99ca73e4c61ee8e346d065",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f0ad01dcde6f03d1d56",
+    "url": "/monsters-rolodex/static/css/main.90df7ca4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0ad01dcde6f03d1d56",
+    "url": "/monsters-rolodex/static/js/main.f1cacb3e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
